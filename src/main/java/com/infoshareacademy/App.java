b149ea6b3691b28cded6c1aaf579723cae1864f5
Kacper Lutynski <kacper.lutynski@gmail.com>
2019-08-14 16:46:49 +0200

    Menu Breadcrumbs v0.2

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -5,6 +5,16 @@ import java.io.IOException;
 public class App {
 
     public static void main(String[] args) throws IOException {
-        new NewMenu.Menu().mainMenu();
+        BreadcrumbHistory bh = new BreadcrumbHistory();
+        bh.addToHistory("dzien ");
+        bh.addToHistory("dobry");
+        bh.addToHistory("krolewno ");
+        System.out.println(bh.toString());
+        bh.removeLast();
+        bh.removeLast();
+        System.out.println(bh.toString());
+
+
+        //new NewMenu.Menu().mainMenu();
     }
 }
