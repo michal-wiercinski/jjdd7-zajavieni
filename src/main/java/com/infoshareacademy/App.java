39c9875c8d616d8ac9f04af555b727f9a896512a
Kacper Lutynski <kacper.lutynski@gmail.com>
2019-08-16 15:59:27 +0200

    Menu Breadcrumbs v0.2

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class App {
 
     public static void main(String[] args) throws IOException {
-        BreadcrumbHistory bh = new BreadcrumbHistory();
+        /*BreadcrumbHistory bh = new BreadcrumbHistory();
         bh.addToHistory("dzien ");
         bh.addToHistory("dobry");
         bh.addToHistory("krolewno ");
@@ -13,8 +13,8 @@ public class App {
         bh.removeLast();
         bh.removeLast();
         System.out.println(bh.toString());
+        */
 
-
-        //new NewMenu.Menu().mainMenu();
+        new NewMenu.Menu().mainMenu();
     }
 }
--- src/main/java/com/infoshareacademy/NewMenu.java
+++ src/main/java/com/infoshareacademy/NewMenu.java
@@ -7,10 +7,12 @@ import java.util.Scanner;
 public class NewMenu {
 
     public static class Menu {
-        MenuBreadcrumbs menuBreadcrumbs = new MenuBreadcrumbs();
+        //MenuBreadcrumbs menuBreadcrumbs = new MenuBreadcrumbs();
+        BreadcrumbHistory bh = new BreadcrumbHistory();
 
         public String mainMenu() throws IOException {
-            System.out.println(menuBreadcrumbs.breadcrumbs);
+            //System.out.println(menuBreadcrumbs.breadcrumbs);
+            System.out.println(bh.toString());
             System.out.println();
             System.out.println("     ****************************************");
             System.out.println("     *                 MENU                 *");
@@ -31,17 +33,18 @@ public class NewMenu {
                 case "1":
                     EventService eventService = new EventService();
                     eventService.printListOfEvents();
-                    menuBreadcrumbs.breadcrumbs += "1. Lista wszystkich wydarzeń -> ";
+                    //menuBreadcrumbs.breadcrumbs += "1. Lista wszystkich wydarzeń -> ";
+                    bh.addToHistory("1. Lista wszystkich wydarzeń -> ");
                     comebackToChoice(choice);
                     break;
                 case "2":
                     System.out.println("Tu będziesz mógł wyszukać wydarzenie.");
-                    menuBreadcrumbs.breadcrumbs += "2. Wyszukaj wydarzenie -> ";
+                    //menuBreadcrumbs.breadcrumbs += "2. Wyszukaj wydarzenie -> ";
                     comebackToChoice(choice);
                     break;
                 case "3":
                     System.out.println("Tu będzie lista Twoich ulubionych wydarzeń.");
-                    menuBreadcrumbs.breadcrumbs += "3. Twoje ulubione wydarzenia -> ";
+                    //menuBreadcrumbs.breadcrumbs += "3. Twoje ulubione wydarzenia -> ";
                     comebackToChoice(choice);
                     break;
                 case "0":
@@ -63,7 +66,8 @@ public class NewMenu {
                 menuBreadcrumbs.breadcrumbs = "";
                 choiceMenu("0");
             } else if (yesOrNot.equalsIgnoreCase("t")) {
-                menuBreadcrumbs.breadcrumbs = "";
+                bh.removeLast();
+                //menuBreadcrumbs.breadcrumbs = "";
                 mainMenu();
             }
         }
