0ef53c6b1bb35c34cfbc7472a11a04af95ccc8fb
Kacper Lutynski <kacper.lutynski@gmail.com>
2019-08-16 16:45:34 +0200

    Menu with breadcrumbs

--- src/main/java/com/isa/zajavieni/App.java
+++ src/main/java/com/isa/zajavieni/App.java
@@ -1,20 +1,13 @@
-package com.infoshareacademy;
+package com.isa.zajavieni;
+
+
+import com.isa.zajavieni.menu.MainMenu;
 
 import java.io.IOException;
 
 public class App {
 
     public static void main(String[] args) throws IOException {
-        /*BreadcrumbHistory bh = new BreadcrumbHistory();
-        bh.addToHistory("dzien ");
-        bh.addToHistory("dobry");
-        bh.addToHistory("krolewno ");
-        System.out.println(bh.toString());
-        bh.removeLast();
-        bh.removeLast();
-        System.out.println(bh.toString());
-        */
-
-        new NewMenu.Menu().mainMenu();
+        new MainMenu().mainMenu();
     }
 }
