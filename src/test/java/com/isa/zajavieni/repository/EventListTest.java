6b90057d38c07d7ea1a56589ebc4fd04182f1286
Michal Wiercinski <michal.wiercinski@int.pl>
2019-08-19 16:20:46 +0200

    JUnit and AssertJ dependencies have been added to pom.xml

---
+++ src/test/java/com/isa/zajavieni/repository/EventListTest.java
@@ -0,0 +1,11 @@
+package com.isa.zajavieni.repository;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventListTest {
+
+}
