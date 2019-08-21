4d45c09e4575af92e4ca53c6b2f69c87733ee977
Maciej Sitarski <maciej.sitarski@onet.pl>
2019-08-21 16:19:38 +0200

    Presentation of events added

--- src/main/java/com/isa/zajavieni/service/EventPrinter.java
+++ src/main/java/com/isa/zajavieni/service/EventPrinter.java
@@ -3,21 +3,41 @@ package com.isa.zajavieni.service;
 import com.isa.zajavieni.jsonclasses.Event;
 
 import java.io.IOException;
+import java.time.Instant;
 import java.util.List;
 
 public class EventPrinter {
 
-    private static final String ANSI_RESET = "\u001B[0m";
-    private static final String ANSI_YELLOW = "\u001b[33;1m";
+  private static final String ANSI_RESET = "\u001B[0m";
+  private static final String ANSI_YELLOW = "\u001b[33;1m";
+  public static final String ANSI_ORANGE = "\u001b[38;5;208m";
 
-    public void printListOfEvents(List<Event> eventList) throws IOException {
-        int counter = 1;
-        for (Event event : eventList) {
-            System.out.println(ANSI_YELLOW + counter + "." + "\tNazwa wydarzenia: " + ANSI_RESET + event.getName() +
-                    ANSI_YELLOW + "\n\tMiejsce: " + ANSI_RESET + event.getPlace().getName()
-                    + ANSI_YELLOW + "\n\tData startu: " + ANSI_RESET + event.getStartDate() + ANSI_YELLOW
-                    + "\n\tNazwa Organizatora: " + ANSI_RESET + event.getOrganizer().getDesignation() + "\n");
-            counter++;
-        }
+  public void printListOfEvents(List<Event> eventList) throws IOException {
+    long now = Instant.now().toEpochMilli();
+    int counter = 1;
+    for (Event event : eventList) {
+      if (event.getStartDate().getTime() - now <= 172800000) {
+        System.out.println(
+            ANSI_ORANGE + counter + "." + "\tNazwa wydarzenia: " + ANSI_RESET + event.getName() +
+                ANSI_ORANGE + "\n\t\tMiejsce: " + ANSI_RESET + event.getPlace().getName()
+                + ANSI_ORANGE + "\n\t\tData startu: " + ANSI_RESET + event.getStartDate() + ANSI_ORANGE
+                + "\n\t\tNazwa Organizatora: " + ANSI_RESET + event.getOrganizer().getDesignation() + "\n");
+        counter++;
+      } else if (event.getStartDate().getTime() - now > 172800000
+          && event.getStartDate().getTime() - now <= 604800000) {
+        System.out.println(
+            ANSI_YELLOW + counter + "." + "\tNazwa wydarzenia: " + ANSI_RESET + event.getName() +
+                ANSI_YELLOW + "\n\t\tMiejsce: " + ANSI_RESET + event.getPlace().getName() + ANSI_YELLOW
+                + "\n\t\tData startu: " + ANSI_RESET + event.getStartDate() + ANSI_YELLOW
+                + "\n\t\tNazwa Organizatora: " + ANSI_RESET + event.getOrganizer().getDesignation() + "\n");
+        counter++;
+      } else {
+        System.out
+            .println(counter + "." + "\tNazwa wydarzenia: " + event.getName() + "\n\t\tMiejsce: "
+                + event.getPlace().getName() + "\n\t\tData startu: " + event.getStartDate()
+                + "\n\t\tNazwa Organizatora: " + event.getOrganizer().getDesignation() + "\n");
+        counter++;
+      }
     }
+  }
 }
