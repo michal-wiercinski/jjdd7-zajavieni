e2d21bb6da077f962536c8e95f8cdcbfff2d50c4
michal <michal.wiercinski@int.pl>
2019-08-22 07:43:57 +0200

    EventSearchTest

--- src/test/java/com/isa/zajavieni/service/EventSearchTest.java
+++ src/test/java/com/isa/zajavieni/service/EventSearchTest.java
@@ -2,12 +2,16 @@ package com.isa.zajavieni.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.isa.zajavieni.jsonclasses.Event;
+import com.isa.zajavieni.repository.EventList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class EventSearchTest {
 
   @Test
   void searchInListByEventName() {
+
   }
 
   @Test
