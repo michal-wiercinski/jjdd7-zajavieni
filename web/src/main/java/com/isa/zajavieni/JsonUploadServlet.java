3f42ec472318ab9e694c972ea3bb1b8a67b10668
Anna Ossowska <ossowska85@gmail.com>
2019-09-03 16:57:35 +0200

    create JsonUploadFile servlet

---
+++ web/src/main/java/com/isa/zajavieni/JsonUploadServlet.java
@@ -0,0 +1,16 @@
+package com.isa.zajavieni;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/upload-file")
+public class JsonUploadServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+
+    }
+
+}
