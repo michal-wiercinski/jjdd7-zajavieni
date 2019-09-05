3cab8e05d3a1c25764542c97df35baf58175e086
Anna Ossowska <ossowska85@gmail.com>
2019-09-05 16:58:15 +0200

    upload json file

--- web/src/main/java/com/isa/zajavieni/servlet/FormUploadServlet.java
+++ web/src/main/java/com/isa/zajavieni/servlet/FormUploadServlet.java
@@ -1,16 +1,33 @@
 package com.isa.zajavieni.servlet;
 
+import com.isa.zajavieni.provider.TemplateProvider;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import javax.inject.Inject;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @WebServlet("/upload-file")
 public class FormUploadServlet extends HttpServlet {
 
+
+    @Inject
+    private TemplateProvider templateProvider;
+
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response){
-
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Template template = templateProvider.getTemplate(getServletContext(), "data-upload.ftlh");
+        Map<String, Object> model = new HashMap<>();
+        try {
+            template.process(model, response.getWriter());
+        } catch (TemplateException e) {
+            e.printStackTrace();
+        }
     }
-
 }
