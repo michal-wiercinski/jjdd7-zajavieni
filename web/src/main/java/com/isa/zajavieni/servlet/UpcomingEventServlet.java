83d2c339e1b54c8f454881e10c0d2955cba52f18
Michal Wiercinski <michal.wiercinski@int.pl>
2019-09-09 16:57:15 +0200

    added link to page of upcoming events

--- web/src/main/java/com/isa/zajavieni/servlet/UpcomingEventServlet.java
+++ web/src/main/java/com/isa/zajavieni/servlet/UpcomingEventServlet.java
@@ -34,17 +34,19 @@ public class UpcomingEventServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html;charset=UTF-8");
         LocalDate currentDate = LocalDate.now();
-        LocalDate endDate = currentDate.plusWeeks(1);
         parseService.parsePlaces("/home/mwiercinski/jjdd7-zajavieni/places.json");
         List<Event> events = parseService.parseEvents("/home/mwiercinski/jjdd7-zajavieni/new_base.json")
                 .stream()
                 .filter(e -> {
-                    LocalDate eventDate = e.getStartDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+                    LocalDate eventDate = e.getEndDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
                     return eventDate.isBefore(currentDate.minusDays(1));
                 }).limit(30)
                 .collect(Collectors.toList());
         Template template = templateProvider.getTemplate(getServletContext(), "upcoming-events.ftlh");
         Map<String, Object> model = new HashMap<>();
+
+
+
         model.put("date", currentDate);
         model.put("events", events);
 
