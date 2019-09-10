033285b9f196adb72dc832c0852c2b13c629f2ed
Maciej Sitarski <maciej.sitarski@onet.pl>
2019-09-10 15:09:41 +0200

    CategoryTypeMapper class has been added

---
+++ web/src/main/java/com/isa/zajavieni/mapper/CategoryTypeMapper.java
@@ -0,0 +1,22 @@
+package com.isa.zajavieni.mapper;
+
+import com.isa.zajavieni.Entity.CategoryType;
+import javax.ejb.Stateless;
+
+@Stateless
+public class CategoryTypeMapper {
+
+  public CategoryType mapCategoryTypeToEntity(
+      com.isa.zajavieni.jsonclasses.CategoryType apiCategoryType) {
+
+    if (apiCategoryType == null) {
+      return null;
+    }
+
+    CategoryType categoryType = new CategoryType();
+    categoryType.setId(apiCategoryType.getId());
+    categoryType.setName(apiCategoryType.getName());
+
+    return categoryType;
+  }
+}
