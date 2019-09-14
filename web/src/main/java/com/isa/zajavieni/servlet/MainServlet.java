93d77275d421537641923202d765c5ccd84ab47b
michal.wiercinski <michal.wiercinski@int.pl>
2019-09-14 17:49:58 +0200

    it works

--- web/src/main/java/com/isa/zajavieni/servlet/MainServlet.java
+++ web/src/main/java/com/isa/zajavieni/servlet/MainServlet.java
@@ -24,7 +24,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-@WebServlet("/main")
+@WebServlet("/")
 public class MainServlet extends HttpServlet {
     private Logger logger = LoggerFactory.getLogger(getClass().getName());
 
