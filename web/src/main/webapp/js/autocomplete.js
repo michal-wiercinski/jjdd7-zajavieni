20405b1f177802ad405d23eab847e461eb5789d6
Anna Ossowska <ossowska85@gmail.com>
2019-09-19 16:56:55 +0200

    livesearch - with noResult bug

--- web/src/main/webapp/js/autocomplete.js
+++ web/src/main/webapp/js/autocomplete.js
@@ -1,24 +1,38 @@
 $("#search").autocomplete({
-   source: function(request, response) {
+    source: function (request, response) {
         $.ajax({
-           url: '/search?phrase=' + request.term,
-           type: 'GET',
-           success: function (data) {
-               let result = data.map(
-                    r => {
-                       return {
-                             id: r.id,
-                             label: r.name,
-                             value: r.name
-                        };
-                    }
-               );
-                response(result);
-           }
+            url: '/search?phrase=' + request.term,
+            type: 'GET',
+            success: function (data) {
+                if (!data.length) {
+                    let result = [
+                        {
+                            label: noResult,
+                            value: 'Brak wyników'
+                        }
+                    ];
+                    response(result);
+                } else {
+                    let result = data.map(
+                        r => {
+                            return {
+                                id: r.id,
+                                label: r.name,
+                                value: r.name
+                            };
+                        }
+                    );
+                    response(result);
+                }
+            }
         });
-   },
-   minLength: 3,
-   select: function(event, ui) {
-        window.location.href = "/event-view?id=" + ui.item.id;
-   }
+    },
+    minLength: 3,
+    select: function (event, ui) {
+        if (ui.item.id === undefined) {
+            return;
+        } else {
+            window.location.href = "/event-view?id=" + ui.item.id;
+        }
+    }
 });
