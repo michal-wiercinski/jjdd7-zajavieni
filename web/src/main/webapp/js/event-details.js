d5b9df4a12725ecb8ad85d9ff0a1611af58bede4
Kacper Lutynski <kacper.lutynski@gmail.com>
2019-09-23 15:50:53 +0200

    editing view of modal

--- web/src/main/webapp/js/event-details.js
+++ web/src/main/webapp/js/event-details.js
@@ -5,7 +5,33 @@ function fetchEventDetails(event, that) {
         type: 'GET',
         success: function (data) {
             $("#event_modal_" + that.modal_event_id.value + " .modal-body .card-text#dupa").first().text(data.id);
+            $("#event_modal_" + that.modal_event_id.value + " .modal-body .card-text#name").first().text(data.name);
+            $("#event_modal_" + that.modal_event_id.value + " .modal-body .card-text#addressName").first().text(data.addressName);
+            $("#event_modal_" + that.modal_event_id.value + " .modal-body .card-text#addressCity").first().text(data.addressCity);
+            $("#event_modal_" + that.modal_event_id.value + " .modal-body .card-text#addressStreet").first().text(data.addressStreet);
+            $("#event_modal_" + that.modal_event_id.value + " .modal-body .card-text#addressZipCode").first().text(data.addressZipCode);
+            $("#event_modal_" + that.modal_event_id.value + " .modal-body .card-text#descShort").first().text(data.descShort);
+            $("#event_modal_" + that.modal_event_id.value + " .modal-body .card-text#descLong").first().text(data.descLong);
+            $("#event_modal_" + that.modal_event_id.value + " .modal-body .card-text#wwwAddress").first().text(data.wwwAddress);
+            $("#event_modal_" + that.modal_event_id.value + " .modal-body .card-text#fbSite").first().text(data.fbSite);
+            $("#event_modal_" + that.modal_event_id.value + " .modal-body .card-text#websiteWithTickets").first().text(data.websiteWithTickets);
+            $("#event_modal_" + that.modal_event_id.value + " .modal-body .card-text#organizerName").first().text(data.organizerName);
         }
     });
     return false;
 };
+/*
+private Long id;
+private String name;
+private Date startDate;
+private Date endDate;
+private String addressName;
+private String addressCity;
+private String addressStreet;
+private String addressZipCode;
+private String descShort;
+private String descLong;
+private String wwwAddress;
+private String fbSite;
+private String websiteWithTickets;
+private String organizerName;*/
