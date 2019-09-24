fbe40b409ef092dabafda61ca5e1079c033646c8
Michal Wiercinski <michal.wiercinski@int.pl>
2019-09-24 12:53:47 +0200

    method UPDATE user in dao has been created, now we can manage privileges

--- web/src/main/java/com/isa/zajavieni/dao/UserDaoBean.java
+++ web/src/main/java/com/isa/zajavieni/dao/UserDaoBean.java
@@ -2,6 +2,7 @@ package com.isa.zajavieni.dao;
 
 import com.isa.zajavieni.entity.User;
 import com.isa.zajavieni.servlet.LoggerServlet;
+
 import java.util.List;
 import java.util.Optional;
 import javax.ejb.Stateless;
@@ -9,57 +10,58 @@ import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Stateless
 public class UserDaoBean {
 
-  private Logger logger = LoggerFactory.getLogger(LoggerServlet.class.getName());
+    private Logger logger = LoggerFactory.getLogger(LoggerServlet.class.getName());
 
-  @PersistenceContext
-  EntityManager entityManager;
+    @PersistenceContext
+    EntityManager entityManager;
 
-  public List<User> findAll(){
-    Query query = entityManager.createNamedQuery("User.findAll", User.class);
-    return query.getResultList();
-  }
-
-  public void saveUser(User user){
-    logger.info("Object user id: {} persist to DB", user.getId());
-    entityManager.persist(user);
-  }
-
-  public void updateUser(User user){
-    entityManager.merge(user);
-  }
-
-  public User findById(Long id) {
-    logger.info("Object event id: {} has been found", id);
-    return entityManager.find(User.class, id);
-  }
-
-  public Optional<User> findByEmail(String email){
-    logger.info("Object User: {} has been found", email);
-    Query query = entityManager.createNamedQuery("User.findByEmail");
-    query.setParameter("email", email);
-    try{
-      return Optional.of((User)query.getSingleResult());
-    }catch (NoResultException e){
-      logger.warn("User for email {} not found", email);
-      return Optional.empty();
+    public List<User> findAll() {
+        Query query = entityManager.createNamedQuery("User.findAll", User.class);
+        return query.getResultList();
     }
-  }
 
-  public void saveEvent(User user) {
-    logger.info("Object event id: {} persist to DB", user.getId());
-    entityManager.persist(user);
-  }
+    public void saveUser(User user) {
+        logger.info("Object user id: {} persist to DB", user.getId());
+        entityManager.persist(user);
+    }
 
-  public List<User> findUsersWithFavouriteEvents(Long id) {
-    Query query = entityManager.createNamedQuery("User.findUsersWithFavouriteEvents");
-    query.setParameter("id", id);
-    return query.getResultList();
-  }
+    public void updateUser(User user) {
+        entityManager.merge(user);
+    }
+
+    public User findById(Long id) {
+        logger.info("Object event id: {} has been found", id);
+        return entityManager.find(User.class, id);
+    }
+
+    public Optional<User> findByEmail(String email) {
+        logger.info("Object User: {} has been found", email);
+        Query query = entityManager.createNamedQuery("User.findByEmail");
+        query.setParameter("email", email);
+        try {
+            return Optional.of((User) query.getSingleResult());
+        } catch (NoResultException e) {
+            logger.warn("User for email {} not found", email);
+            return Optional.empty();
+        }
+    }
+
+    public void saveEvent(User user) {
+        logger.info("Object event id: {} persist to DB", user.getId());
+        entityManager.persist(user);
+    }
+
+    public List<User> findUsersWithFavouriteEvents(Long id) {
+        Query query = entityManager.createNamedQuery("User.findUsersWithFavouriteEvents");
+        query.setParameter("id", id);
+        return query.getResultList();
+    }
 
 }
--- web/src/main/java/com/isa/zajavieni/servlet/UserServlet.java
+++ web/src/main/java/com/isa/zajavieni/servlet/UserServlet.java
@@ -16,22 +16,22 @@ import java.util.Optional;
 @WebServlet("/user")
 public class UserServlet extends HttpServlet {
 
-  @EJB
-  UserService userService;
+    @EJB
+    UserService userService;
 
-  @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-      throws ServletException, IOException {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
 
-    String email = req.getSession().getAttribute("email").toString();
-    Optional<UserDto> user = userService.getUserByEmail(email);
-    ObjectMapper objectMapper = new ObjectMapper();
-    if (user.isPresent()) {
-      String json = objectMapper.writeValueAsString(user.get());
-      resp.getWriter().println(json);
-    } else {
-      resp.setStatus(404);
+        String email = req.getSession().getAttribute("email").toString();
+        Optional<UserDto> user = userService.getUserByEmail(email);
+        ObjectMapper objectMapper = new ObjectMapper();
+        if (user.isPresent()) {
+            String json = objectMapper.writeValueAsString(user.get());
+            resp.getWriter().println(json);
+        } else {
+            resp.setStatus(404);
+        }
+
     }
-
-  }
 }
