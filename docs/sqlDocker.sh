7c12859ad150f083c393404e344d8bc1e813fe19
Anna Ossowska <ossowska85@gmail.com>
2019-09-27 16:57:01 +0200

    search event

--- docs/sqlDocker.sh
+++ docs/sqlDocker.sh
@@ -1,2 +1,2 @@
-#!/bin/sh
+#!/bin/shd63d2260b15a97e534c
 docker run -p 0.0.0.0:3309:3306 --name mysql-project-base -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=eventsdb -d mysql
--- web/src/main/java/com/isa/zajavieni/servlet/AdvancedSearchServlet.java
+++ web/src/main/java/com/isa/zajavieni/servlet/AdvancedSearchServlet.java
@@ -1,28 +1,30 @@
 package com.isa.zajavieni.servlet;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.isa.zajavieni.dto.EventDto;
 import com.isa.zajavieni.provider.TemplateProvider;
+import com.isa.zajavieni.service.FavouriteEventService;
 import com.isa.zajavieni.service.SearchService;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import javax.inject.Inject;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @WebServlet("/advanced-search")
 public class AdvancedSearchServlet extends HttpServlet {
+
+  private Logger logger = LoggerFactory.getLogger(getClass().getName());
 
   @Inject
   private SearchService searchService;
@@ -30,11 +32,22 @@ public class AdvancedSearchServlet extends HttpServlet {
   @Inject
   private TemplateProvider templateProvider;
 
+  @Inject
+  private FavouriteEventService favouriteEventService;
+
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
     Template template = templateProvider
         .getTemplate(getServletContext(), "advanced-search.ftlh");
     Map<String, Object> model = new HashMap<>();
+    Long userId = (Long) req.getSession().getAttribute("userId");
+
+    if (userId != null) {
+      List<EventDto> favouriteEvents = favouriteEventService
+          .findListOfUserFavouriteEventsDto(userId);
+      favouriteEventService.displayFavouriteEventBeam(req, favouriteEvents, model);
+      model.put("userId", userId);
+    }
     try {
       template.process(model, resp.getWriter());
     } catch (TemplateException e) {
@@ -70,9 +83,25 @@ public class AdvancedSearchServlet extends HttpServlet {
         events = searchService.searchEventsByNameAndStartDate(name, startDate);
       }
     }
-    ObjectMapper objectMapper = new ObjectMapper();
-    String json = objectMapper.writeValueAsString(events);
-    resp.getWriter().println(json);
+
+    Template template = templateProvider.getTemplate(getServletContext(), "search.ftlh");
+
+    Map<String, Object> model = new HashMap<>();
+    model.put("events", events);
+    Long userId = (Long) req.getSession().getAttribute("userId");
+
+    if (userId != null) {
+      List<EventDto> favouriteEvents = favouriteEventService
+          .findListOfUserFavouriteEventsDto(userId);
+      favouriteEventService.displayFavouriteEventBeam(req, favouriteEvents, model);
+      model.put("userId", userId);
+    }
+
+    try {
+      template.process(model, resp.getWriter());
+    } catch (TemplateException e) {
+      logger.error(e.getMessage());
+    }
 
   }
 }
